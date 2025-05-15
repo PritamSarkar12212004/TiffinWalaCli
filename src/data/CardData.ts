ad3880fa63140f080b5a23aae51aa6dbf41c6b7e
Pritam Sarkar <pritam7796419792@gmail.com>
2025-05-16 01:23:17 +0530

    first commit

---
+++ src/data/CardData.ts
@@ -0,0 +1,18 @@
+const Data = [
+  {
+    name: 'card 1',
+    color: 'red',
+    item: 'this is my frist card',
+  },
+  {
+    name: 'card 1',
+    color: 'red',
+    item: 'this is my frist card',
+  },
+  {
+    name: 'card 1',
+    color: 'red',
+    item: 'this is my frist card',
+  },
+];
+export default Data;
